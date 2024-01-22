df523bed9013f544ee3566cb0ef55ae8babe8317
AyyPeff <log.alexpeffer@gmail.com>
2024-01-22 15:22:50 -0600

    Game now starts upon first key press

--- smokeCrack.js
+++ smokeCrack.js
@@ -28,38 +28,6 @@ const modalMessage = document.getElementById('modalMessage');
 const modalRetryBtn = document.getElementById('retryBtn');
 
 
-let iterator = 0;
-
-document.body.addEventListener("keydown", (event)=> {
-    var spanNum = document.getElementById(`span${iterator + 1}`);
-    if (myModal.style.display != "block") {
-        if (event.key == selectedChars[iterator]){
-            console.log("correct " + selectedChars[iterator])
-            spanNum.style.backgroundColor = "#0FCFDD";
-            spanNum.style.boxShadow = "0px 4px 0px rgb(10, 115, 122)"
-            iterator++;
-
-            if(iterator >= 15) {
-                modalMessage.textContent = "CONGRATULATIONS! YOU'RE IN!"
-                myModal.style.display = "block";
-                clearInterval(timerInterval);
-            } 
-
-            return;
-        } else{
-            modalMessage.textContent = "YOU FAILED! (reason: wrong keypress)"
-            myModal.style.display = "block";
-            clearInterval(timerInterval);
-        }
-    }
-    // if you press enter while modal is diplayed, it will refresh the page for another try
-    if (myModal.style.display == "block"){
-        if (event.key == "Enter") {
-            location.reload();
-        }
-    }
-})
-
 // set duration to 5 seconds
 const timerDuration = 5;
 
@@ -79,22 +47,57 @@ let currentWidth = 100;
 
 const texttimer = document.getElementById("texttimer");
 
-// update the timer at the intervals described
+let iterator = 0;
 
-const timerInterval = setInterval(() => {
-    currentWidth -= widthIncrement;
-    time.style.width = currentWidth + '%';
+let timerInterval;
 
-    timeRemaining -= timeIncrement;
-    texttimer.textContent = timeRemaining + " seconds";
 
-    if (currentWidth <= 0) {
-        clearInterval(timerInterval);
-        modalMessage.textContent = "YOU FAILED! (reason: end of timer)"
-        myModal.style.display = "block";
+document.body.addEventListener("keydown", (event)=> {
+    var spanNum = document.getElementById(`span${iterator + 1}`);
+    if (myModal.style.display != "block") {
+        if (event.key == selectedChars[iterator]){
+
+            if(iterator >= 14) {
+                modalMessage.textContent = "CONGRATULATIONS! YOU'RE IN!"
+                myModal.style.display = "block";
+                clearInterval(timerInterval);
+            } 
+
+            // update the timer at the intervals described
+            if (iterator === 0){
+                    timerInterval = setInterval(() => {
+                    currentWidth -= widthIncrement;
+                    time.style.width = currentWidth + '%';
+        
+                    timeRemaining -= timeIncrement;
+                    texttimer.textContent = timeRemaining + " seconds";
+
+                    if (currentWidth <= 0) {
+                        clearInterval(timerInterval);
+                        modalMessage.textContent = "YOU FAILED! (reason: end of timer)"
+                        myModal.style.display = "block";
+                    }                
+                }, updateInterval);
+            }
+
+            console.log("correct " + selectedChars[iterator])
+            spanNum.style.backgroundColor = "#0FCFDD";
+            spanNum.style.boxShadow = "0px 4px 0px rgb(10, 115, 122)"
+            iterator++;
+            return;
+        } else{
+            modalMessage.textContent = "YOU FAILED! (reason: wrong keypress)"
+            myModal.style.display = "block";
+            clearInterval(timerInterval);
+        }
     }
-}, updateInterval);
-
+    // if you press enter while modal is diplayed, it will refresh the page for another try
+    if (myModal.style.display == "block"){
+        if (event.key == "Enter") {
+            location.reload();
+        }
+    }
+})
 
 
 
